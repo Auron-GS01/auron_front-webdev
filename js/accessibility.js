f557e12da7bf5a37184d0b54bff1479352dda70f
esther <tozzo.est@gmail.com>
2026-06-05 22:08:47 -0300

    Atualizando funcionalidades de alternância de cor (logo e animação)

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,7 +1,16 @@
 const themeButton = document.getElementById("themeToggle");
 
+let currentTheme = "dark";
+
 themeButton.addEventListener("click", () => {
 
-    document.body.classList.toggle("light-theme");
-
+    if(currentTheme === "dark") {
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+        currentTheme = "light";
+    } else {
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+        currentTheme = "dark";
+    }
 });
