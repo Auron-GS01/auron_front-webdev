a701337588680f5b19d4c45251f7f6ea246f4df1
Izabela Pordeus <145785503+ibpds@users.noreply.github.com>
2026-06-08 12:57:28 -0300

    feat(quiz): adicionar variáveis de controle do quiz
    Added variables to track current question and score.

--- js/quiz.js
+++ js/quiz.js
@@ -2,3 +2,6 @@ const questions = document.querySelectorAll(".quiz_question");
 const resultBox = document.querySelector(".quiz_result");
 const resultText = document.querySelector(".result_text");
 const restartBtn = document.querySelector(".restart");
+
+let currentQuestion = 0;
+let score = 0;
