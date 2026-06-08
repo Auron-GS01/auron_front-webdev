2d6010eee456685be2131cdd9473a0701a0ba34f
Izabela Pordeus <145785503+ibpds@users.noreply.github.com>
2026-06-08 12:56:39 -0300

    feat(quiz): criar seleção inicial dos elementos do quiz

---
+++ js/quiz.js
@@ -0,0 +1,4 @@
+const questions = document.querySelectorAll(".quiz_question");
+const resultBox = document.querySelector(".quiz_result");
+const resultText = document.querySelector(".result_text");
+const restartBtn = document.querySelector(".restart");
