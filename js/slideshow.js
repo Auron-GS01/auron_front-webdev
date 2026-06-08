e5d5c681b16719ef984d7c8f6b5cc112b2f01b7a
FelZimmer <fozimermann@gmail.com>
2026-06-08 11:11:02 -0300

    feat(slideshow):Criar estrutura inicial do slider,Controles next e prev, e variavel de controle do slide atual

---
+++ js/slideshow.js
@@ -0,0 +1,8 @@
+
+const sliderTrack = document.querySelector(".slider_track");
+const slides = document.querySelectorAll(".slides");
+
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let currentSlide = 0;
