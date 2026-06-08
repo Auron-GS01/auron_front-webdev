bff254dd2661a3d1bafc58da6b0525b1e805ab3f
FelZimmer <fozimermann@gmail.com>
2026-06-08 11:20:55 -0300

    feat(slideshow): implementar função updateSlider, adicionar evento de avanco do slider, corrigir limite maximo dos slides

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,3 +6,23 @@ const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 
 let currentSlide = 0;
+
+function updateSlider() {
+
+    sliderTrack.style.transform =
+    `translateX(-${currentSlide * 100}%)`;
+}
+
+
+nextBtn.addEventListener("click", () => {
+
+    currentSlide++;
+
+
+    if(currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    updateSlider();
+});
+
